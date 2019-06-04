52f08936db061b44b0410290c30beb862fb5eb32
ufrarion <cassianodab@gmail.com>
2019-06-03 22:46:30 -0300

    Corrigindo Cable

--- app/controllers/interface_controller.rb
+++ app/controllers/interface_controller.rb
@@ -8,7 +8,7 @@ class InterfaceController < ApplicationController
   end
 
   def scan
-    ActionCable.server.broadcast("room_1", code: Digest::SHA1.hexdigest(DateTime.now.strftime("%Q")))
+    ActionCable.server.broadcast("room_1", code: Digest::SHA1.hexdigest(DateTime.now.strftime("%Q")), message: params[:code])
     respond_with(msg: "SERVER GET RESPONSE")
   end
 end
